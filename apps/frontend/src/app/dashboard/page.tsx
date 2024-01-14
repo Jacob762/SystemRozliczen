79c82c950bcccf7c05cf6b2b7818fdfd8332a9f0
Michał Korczak <michal.k2001@gmail.com>
2024-01-14 03:48:24 +0100

    feat: any

--- apps/frontend/src/app/dashboard/page.tsx
+++ apps/frontend/src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { css } from 'styled-system/css';
 import { Stack } from 'styled-system/jsx';
 import { getDocuments } from '~/api/getDocuments';
+import { getStatistics } from '~/api/getStatistics';
 import LatestDocuments from '~/components/dashboard/LatestDocuments';
 import Statistics from '~/components/dashboard/Statistics';
 
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 
 export default async function Dashboard() {
   const initialData = await getDocuments();
+  const statisticsData = await getStatistics();
 
   return (
     <>
@@ -22,7 +24,7 @@ export default async function Dashboard() {
         gap="5"
         className={css({ fontSize: '2xl', fontWeight: 'bold' })}
       >
-        <Statistics />
+        <Statistics statistics={statisticsData} />
         <LatestDocuments documents={initialData} />
       </Stack>
     </>
--- apps/frontend/src/components/dashboard/Statistics.tsx
+++ apps/frontend/src/components/dashboard/Statistics.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
 import { css } from 'styled-system/css';
 import { Stack } from 'styled-system/jsx';
-import * as Card from '~/components/ui/card';
+import { getStatistics } from '~/api/getStatistics';
 import { Button } from '~/components/ui/button';
+import * as Card from '~/components/ui/card';
 
-export default function Statistics() {
+export default function Statistics(props: { statistics: any }) {
+  const { data } = useQuery({
+    queryKey: ['statistics'],
+    queryFn: getStatistics,
+    initialData: props.statistics,
+  });
+
   return (
     <Card.Root width="lg">
       <Card.Header>
@@ -18,23 +28,24 @@ export default function Statistics() {
           <Stack direction="row" gap="4">
             <div className={css({ flex: '1' })}>
               <Card.Title>Liczba dokumentow</Card.Title>
-              <Card.Description>123</Card.Description>
-            </div>{' '}
+              <Card.Description>{data.dokumenty}</Card.Description>
+            </div>
             <div className={css({ flex: '1' })}>
               <Card.Title>Użytkownicy</Card.Title>
-              <Card.Description>123</Card.Description>
+              <Card.Description>{data.pracownicy}</Card.Description>
             </div>
           </Stack>
           <Stack direction="row" gap="4">
             <div className={css({ flex: '1' })}>
               <Card.Title>Ksiegowi</Card.Title>
-              <Card.Description>123</Card.Description>
+              <Card.Description>{data.ksiegowi}</Card.Description>
             </div>
             <div className={css({ flex: '1' })}>
               <Card.Title>Laczna kwota</Card.Title>
-              <Card.Description>123</Card.Description>
-            </div>{' '}
-            {/*statystyka/{id} ,gdzie id to id organizacji, zwraca string ze wszystkimi danymi potrzebnymi do uzupelnienia, StatystykaController*/}
+              <Card.Description>
+                {Math.round((data.kwota + Number.EPSILON) * 100) / 100} zł
+              </Card.Description>
+            </div>
           </Stack>
         </Stack>
       </Card.Body>
